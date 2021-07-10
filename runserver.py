77bc03526cc11c54a0b346776711d80392d0ea5a
Louis Young <1462648167>
2021-07-10 18:05:41 +0800

    <feat>(Server): Add multiprocess model

--- runserver.py
+++ runserver.py
@@ -4,8 +4,10 @@ from Scheduler.scheduler import schedule
 from Config.settings import config
 from Server.server import server
 from threading import Thread
+from concurrent.futures import ProcessPoolExecutor
 
 def running():
+    PROCESS_MODEL = config.settings("Server", "PROCESS_MODEL")
     SCHEDULER = config.settings("Scheduler", "START_USING")
     if SCHEDULER == False:
         thread_main = Thread(target=main)
@@ -13,9 +15,17 @@ def running():
     else: # 调度器开启后main函数将被scheduler调度器代理，开启定时执行main
         thread_scheduler = Thread(target=schedule)
         thread_scheduler.start()
+    if PROCESS_MODEL:
+        work_count = config.settings("Server", "PROCESS_COUNT")
+        server_process(work_count)
+    else:
+        thread_server = Thread(target=server)
+        thread_server.start()
 
-    thread_server = Thread(target=server)
-    thread_server.start()
+def server_process(work_count=4):
+    with ProcessPoolExecutor(work_count) as pool:
+        for i in range(work_count):
+            pool.submit(server())
 
 if __name__ == "__main__":
     DEBUG = config.settings("Debug", "DEBUG")
