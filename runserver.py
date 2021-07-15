4af5755f52d53add3de6f4bf88ccd2f4874f9015
Louis Young <1462648167>
2021-07-15 17:52:33 +0800

    <refacor>(scheduler): optimized the overall architecture of the scheduler

--- runserver.py
+++ runserver.py
@@ -1,6 +1,6 @@
 from Core.core import main
 from Logger.logger import logger
-from Scheduler.scheduler import schedule
+from Scheduler.scheduler import Timer
 from Config.settings import config
 from Server.server import server
 from threading import Thread
@@ -13,7 +13,10 @@ def running():
         thread_main = Thread(target=main)
         thread_main.start()
     else: # 调度器开启后main函数将被scheduler调度器代理，开启定时执行main
-        thread_scheduler = Thread(target=schedule)
+        startTime = config.settings("Scheduler", "START_TIME")
+        scheduler = Timer(task=main, startTime=startTime)
+        scheduler.schedule()
+        thread_scheduler = Thread(target=scheduler.schedule)
         thread_scheduler.start()
     if PROCESS_MODEL:
         work_count = config.settings("Server", "PROCESS_COUNT")
