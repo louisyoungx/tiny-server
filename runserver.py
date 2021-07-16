0903849a92d57cdd5b1a143a1812c70480f50845
Louis Young <1462648167>
2021-07-17 01:27:08 +0800

    <fix>(runserver): resolved an issue where the main process was blocked

--- runserver.py
+++ runserver.py
@@ -10,17 +10,17 @@ def running():
     PROCESS_MODEL = config.settings("Server", "PROCESS_MODEL")
     SCHEDULER = config.settings("Scheduler", "START_USING")
     SERVER = config.settings("Server", "START_USING")
-    if SCHEDULER == False:
+    print(SERVER)
+    if SCHEDULER is False:
         thread_main = Thread(target=main)
         thread_main.start()
     else: # 调度器开启后main函数将被scheduler调度器代理，开启定时执行main
         startTime = config.settings("Scheduler", "START_TIME")
         skipWeekend = config.settings("Scheduler", "SKIP_WEEKEND")
         scheduler = Timer(task=main, startTime=startTime, skipWeekend=skipWeekend)
-        scheduler.schedule()
         thread_scheduler = Thread(target=scheduler.schedule)
         thread_scheduler.start()
-    if SERVER:
+    if SERVER is True:
         if PROCESS_MODEL:
             work_count = config.settings("Server", "PROCESS_COUNT")
             server_process(work_count)
@@ -35,7 +35,7 @@ def server_process(work_count=4):
 
 if __name__ == "__main__":
     DEBUG = config.settings("Debug", "DEBUG")
-    if DEBUG == True:
+    if DEBUG is True:
         logger.info("\n===== DEBUG MODE =====")
         main()
     else:
