55721cc1cb1ad92b477757bc788c2fdf7c8e1b4d
louisyoungx <1462648167@qq.com>
2021-12-04 08:02:45 +0800

    feat(logger): progress bar

--- Core/core.py
+++ Core/core.py
@@ -2,9 +2,12 @@ from Storage.storage import storage
 from Config.settings import config
 from Message.message import message
 from Logger.logger import logger
+from Logger.progress import progress
 
 
 def main():
     # start code
     logger.info("===== write code here =====")
+    progress(10)
+
 
