0b0ba30bd04ab0f5c211097e5beadc539e831893
louisyoungx <1462648167@qq.com>
2022-01-19 17:10:34 +0800

    perf(config.direct): adjust for list data

--- Config/direct.py
+++ Config/direct.py
@@ -6,3 +6,12 @@ class Direct(object):
                 exec(f'self.{key} = child')
             else:
                 exec(f'self.{key} = target[key]')
+            if type(target[key]) == list:
+                for item in target[key]:
+                    nList = []
+                    if type(item) == dict:
+                        dItem = Direct(item)
+                        nList.append(dItem)
+                    else:
+                        nList.append(item)
+                exec(f'self.{key} = nList')
